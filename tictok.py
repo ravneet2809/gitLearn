6e1eec61cc969eddf005701dcfa2843aa4d0d8f1
ravneet2809 <41836604+ravneet2809@users.noreply.github.com>
2020-12-17 11:33:52 -0400

    Create tictok.py
    try1

---
+++ tictok.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 14 18:58:21 2020
+
+@author: ravne
+"""
+
+
+
+
+
+#####working on the display module 
+
+
+def display(row1,row2,row3):
+    print(row1)
+    print(row2)
+    print(row3)
+
+
+
+row1=[" "," "," "]
+row2=[" "," "," "]
+row3=[" "," "," "]    
+
+
+
+
+display(row1,row2,row3)
+
+
+
+
+
+
+## accepting the input from the user 
+
+
+
+result=int(input("choose an index value: "))
+
+2+2
